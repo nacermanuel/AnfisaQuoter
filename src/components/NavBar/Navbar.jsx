99539191699e2cd78709f2fbef12d4f6771241d1
Manuel <nacermanuel@gmail.com>
2022-12-16 20:51:35 -0500

    Detalles de overlay en el popcotizador y edicion del NovBar

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import './Navbar.css'
+import { useSelector } from 'react-redux';
 
 const Navbar = () => {
 
+    const productosCotizacion = useSelector(state => state.cotizacion)
+    
+    //AGREGAR AQUI LA POSIBILIDAD DE ACTIVAR EL COTIZADOR
+    // SERA QUE SACO ESTE NAV Y LO LLEVO A HOM EN VEZ DE APP?? 
+
     return (
         <>
-            <nav className='nav'> <h2>TOTAL COTIZADO $XX</h2> <button>Ver cotizacion</button> </nav> 
+            <nav className='nav'> 
+            <h2>Total Productos: $ { ( productosCotizacion.length ? productosCotizacion.map((e) => ( e.cantidad * e.precio )).reduce((sum, acu) => sum + acu).toFixed(2) : 0) }</h2>
+                <button>Ver cotizacion</button> 
+            </nav> 
         </>
     )
 }
--- src/components/PopCotizacion/PopCotizacion.jsx
+++ src/components/PopCotizacion/PopCotizacion.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useSelector, useDispatch } from "react-redux";
 import * as actions from '../../redux/actions'
+import './PopCotizacion.css'
 
 const BACKGROUND_EFFECT = {
     position: 'fixed',
@@ -17,10 +18,16 @@ const BACKGROUND_EFFECT = {
 const CAJA_COTIZACION = {
     backgroundColor: 'rgb(233, 210, 227)',
     width: '900px',
-    height: '600px',
+    height: '95%',
     borderRadius: '7%',
     border: '2px solid #ffffff',
     paddingLeft: '15px',
+
+}
+
+const CAJA_PRODUCTOS = {
+    overflowY: 'scroll',
+    maxHeight: '80%'
 }
 
 const BOTON_CERRAR = {
@@ -29,7 +36,6 @@ const BOTON_CERRAR = {
     marginTop: '15px'
 
 }
-
 
 
 export default function PopCotizacion(props) {
@@ -58,36 +64,38 @@ export default function PopCotizacion(props) {
                 <div style={CAJA_COTIZACION}>
                     <button onClick={props.cerrar} style={BOTON_CERRAR}>X</button>
                     <h2 style={{textAlign: 'center'}}>Cotización de tu pedido:</h2>
-                    <p>este es un cuadro</p>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th></th>
-                                <th>Cant.</th>
-                                <th>Imagen</th>
-                                <th>Nombre</th>
-                                <th>V.Unitario</th>
-                                <th>V.Total</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {                                    
-                                productosCotizacion.map((e,i) => (
-                        
-                                <tr key={i}>
-                                    <td><button onClick={()=> clickEliminar(e.id)}>x</button></td>
-                                    <td><input onChange={(event)=> cambioCantidad(e.id,event)} name={i} type="number" value={e.cantidad} min='1' max='12' style={{width: '32px'}}/></td>
-                                    <td>{e.nombre}</td>
-                                    <td>{e.nombre}</td>
-                                    <td>{e.precio}</td>
-                                    <td>{e.cantidad * e.precio}</td>
-                                </tr> 
-                                
-                                ))
-                            }
-                        </tbody>
-                    </table>
-                    <p>Total Producto: { ( productosCotizacion.length ? productosCotizacion.map((e) => ( e.cantidad * e.precio )).reduce((sum, acu) => sum + acu) : 0) }</p>
+                    <div className='cajaProductos' style={CAJA_PRODUCTOS}>
+                        <table style={{marginLeft: 'auto' , marginRight: 'auto'}}>
+                            <thead>
+                                <tr>
+                                    <th style={{width: '30px', textAlign: 'center'}}></th>
+                                    <th style={{textAlign: 'center'}}>Cant.</th>
+                                    <th style={{textAlign: 'center'}}>Imagen</th>
+                                    <th style={{width: '390px', textAlign: 'center'}} >Nombre</th>
+                                    <th style={{width: '80px', textAlign: 'center'}} >V.Unitario</th>
+                                    <th style={{width: '80px', textAlign: 'center'}}>V.Total</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {   
+                                    productosCotizacion.length  ?                             
+                                    productosCotizacion.map((e,i) => (
+                            
+                                    <tr key={i}>
+                                        <td><button onClick={()=> clickEliminar(e.id)}>x</button></td>
+                                        <td><input onChange={(event)=> cambioCantidad(e.id,event)} name={i} type="number" value={e.cantidad} min='1' max='12' style={{width: '32px'}}/></td>
+                                        <td><img src={require(`../Producto/imagenes/${e.img}`)} alt="" className='imagenMini' /></td>
+                                        <td style={{textAlign: 'center'}} >{e.nombre}</td>
+                                        <td style={{textAlign: 'center'}} >$ {(e.precio).toFixed(2)}</td>
+                                        <td style={{textAlign: 'center', fontWeight: 'bold' }} >$ {(e.cantidad * e.precio).toFixed(2)}</td>
+                                    </tr> 
+                                    
+                                    )) : <td colSpan={6}><p style={{textAlign: 'center', fontWeight: 'bold' }} >No hay productos seleccionados</p></td>
+                                }
+                            </tbody>
+                        </table>
+                        <h3 style={{ textAlign: 'right', width: '88%'}}>Total Producto: $ { ( productosCotizacion.length ? productosCotizacion.map((e) => ( e.cantidad * e.precio )).reduce((sum, acu) => sum + acu).toFixed(2) : 0) }</h3>                    
+                    </div>
                 </div>    
         </div>
     
